ad064dc94f741ad7baf1c38c9a5417ce06a356cc
lucasporfz <38968877+lucasporfz@users.noreply.github.com>
2025-09-22 11:48:01 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// IMPORTANT: set `base` to '/NOME_DO_REPO/' (inclui barra no início e no fim)
+// Ex.: se seu repositório se chama "wheel-bot", use base: '/wheel-bot/'
+export default defineConfig({
+  plugins: [react()],
+  base: '/REPO_NAME/',
+})
